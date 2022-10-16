0f5b3f1c0f77524c72de2dcc4e35a9b80071aba8
Emre3105 <emre.sener@etu.univ-lyon1.fr>
2022-10-16 02:50:44 +0200

    début du template

---
+++ mpp/resources/views/template.blade.php
@@ -0,0 +1,50 @@
+@extends('head')
+@section('js')
+<div class="w-full h-full bg-gray-darkest text-white p-4 space-y-4">
+    <p class="text-4xl w-min mx-auto">Template</p>
+    <hr>
+    <div class="w-full">
+        <p class="text-2xl mb-4 w-min mx-auto">Fontsizes</p>
+        <p class="text-xs">text-xs</p>
+        <p class="text-sm">text-sm</p>
+        <p class="text-base">text-base</p>
+        <p class="text-lg">text-lg</p>
+        <p class="text-xl">text-xl</p>
+        <p class="text-2xl">text-2xl</p>
+        <p class="text-3xl">text-3xl</p>
+        <p class="text-4xl">text-4xl</p>
+        <p class="text-5xl">text-5xl</p>
+    </div>
+    <hr>
+    <div class="w-full">
+        <p class="text-2xl mb-4 w-min mx-auto">Colors</p>
+        <div class="grid gap-2 grid-cols-6">
+            <div class="w-40 h-20 bg-gray-very-lightest flex justify-center items-center text-black"><p>bg-gray-very-lightest</p></div>
+            <div class="w-40 h-20 bg-gray-lightest flex justify-center items-center text-black"><p>bg-gray-lightest</p></div>
+            <div class="w-40 h-20 bg-gray-light flex justify-center items-center"><p>bg-gray-light</p></div>
+            <div class="w-40 h-20 bg-gray flex justify-center items-center"><p>bg-gray</p></div>
+            <div class="w-40 h-20 bg-gray-dark flex justify-center items-center"><p>bg-gray-dark</p></div>
+            <div class="w-40 h-20 bg-gray-darkest flex justify-center items-center border border-gray-dark"><p>bg-gray-darkest</p></div>
+            <div class="w-40 h-20 bg-cyan flex justify-center items-center"><p>bg-cyan</p></div>
+            <div class="w-40 h-20 bg-cyan-dark flex justify-center items-center"><p>bg-cyan-dark</p></div>
+            <div class="w-40 h-20 bg-red-light flex justify-center items-center"><p>bg-red-light</p></div>
+            <div class="w-40 h-20 bg-red flex justify-center items-center"><p>bg-red</p></div>
+            <div class="w-40 h-20 bg-red-dark flex justify-center items-center"><p>bg-red-dark</p></div>
+            <div class="w-40 h-20 bg-green-light flex justify-center items-center"><p>bg-green-light</p></div>
+            <div class="w-40 h-20 bg-green flex justify-center items-center"><p>bg-green</p></div>
+            <div class="w-40 h-20 bg-green-dark flex justify-center items-center"><p>bg-green-dark</p></div>
+        </div>
+    </div>
+    <hr>
+    <div class="w-full">
+        <p class="text-2xl mb-4 w-min mx-auto">Buttons</p>
+        <div class="space-x-4">
+            <button class="btn-primary">btn-primary</button>
+            <button class="btn-secondary">btn-secondary</button>
+            <button class="btn-success">btn-success</button>
+            <button class="btn-danger">btn-danger</button>
+        </div>
+    </div>
+    <hr>
+</div>
+@endsection
--- mpp/tailwind.config.js
+++ mpp/tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-very-lightest': '#E5E7EB',
+        'gray-lightest': '#D1D5DB',
+        'gray-light': '#9CA3AF',
+        'gray': '#6B7280',
+        'gray-dark': '#374151',
+        'gray-darkest': '#1F2937',
+        'cyan': '#155E75',
+        'cyan-dark': '#164E63',
+        'red-light': '#DC2626',
+        'red': '#B91C1C',
+        'red-dark': '#991B1B',
+        'green-light': '#84CC16',
+        'green': '#65A30D',
+        'green-dark': '#4D7C0F',
+      },
+    },
   },
   plugins: [],
 }
