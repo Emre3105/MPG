15057cebc784294c5bf3a2ac9272d5ad3702cec0
p1801601 <emre.sener@etu.univ-lyon1.fr>
2022-12-04 12:31:11 +0100

    migration : create players table

---
+++ mpp/database/migrations/2022_12_04_111844_create_players_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('league_id');
+            $table->boolean('favorite')->default(false);
+            $table->timestamps();
+            
+            $table->unique(['user_id', 'league_id']);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('league_id')->references('id')->on('leagues');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+};
