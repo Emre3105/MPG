04f0d6c50208fddd612382249448fa959b2d1032
p1801601 <emre.sener@etu.univ-lyon1.fr>
2022-12-04 13:02:20 +0100

    fix : PlayerFactory cannot assign a user to the same league

--- mpp/database/factories/PlayerFactory.php
+++ mpp/database/factories/PlayerFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use App\Models\League;
+use Illuminate\Support\Facades\DB;
 
 class PlayerFactory extends Factory
 {
@@ -18,10 +19,18 @@ class PlayerFactory extends Factory
         if(!League::exists()) {
             League::factory(1)->create();
         }
-        
+
+        $userId = 17; //\App\Models\User::all()->random()->id
+        $randomLeague = League::whereNotIn(
+            'id', DB::table('players')->select('league_id')->where('user_id', $userId)
+        )->inRandomOrder()->first();
+
+        if (!$randomLeague) {
+            dd('cet utilisateur est présent dans toutes les ligues');
+        }
         return [
-            'user_id' => \App\Models\User::all()->random()->id,
-            'league_id' => League::all()->random()->id
+            'user_id' => $userId,
+            'league_id' => $randomLeague->id
         ];
     }
 }
