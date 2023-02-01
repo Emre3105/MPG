dc1209a9982258ace893cbcdaa5e48ca0f7d4195
Emre <emre.sener@etu.univ-lyon1.fr>
2023-02-01 23:03:51 +0100

    fix : league not found after joining it

--- mpp/resources/views/league.blade.php
+++ mpp/resources/views/league.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <league-panel
         :league="{{$league}}"
-        :validated="{{$validated ? 'true' : 'false'}}"
+        :validated="{{isset($validated) && $validated ? 'true' : 'false'}}"
         :auth="{{Auth::user()->id}}"
         url-browse-players={{route('league.player.browse', $league->id)}}
         url-launch-league={{route('league.launch', $league->id)}}
