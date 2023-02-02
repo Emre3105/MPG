71d7bb4add368e57ed7279864e16aed09056b313
Emre <emre.sener@etu.univ-lyon1.fr>
2023-02-02 20:11:24 +0100

    feature : create Game model

---
+++ mpp/app/Models/Game.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'home_player_id',
+        'visiting_player_id',
+        'game_number'
+    ];
+}
