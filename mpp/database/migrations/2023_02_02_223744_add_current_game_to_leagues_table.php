ded06590df3ee4745fd9e913b8ddeaa72b41949c
Emre <emre.sener@etu.univ-lyon1.fr>
2023-02-02 23:39:28 +0100

    migration : add_current_game_to_leagues_table

---
+++ mpp/database/migrations/2023_02_02_223744_add_current_game_to_leagues_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('leagues', function (Blueprint $table) {
+            //
+            $table->integer('current_game')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('leagues', function (Blueprint $table) {
+            //
+        });
+    }
+};
